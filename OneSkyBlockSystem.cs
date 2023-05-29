348a4c122801985f53079ff08a4a1d2c10768e71
Dummiez <ryansapan@hotmail.com>
2023-05-29 13:45:29 -0400

    mod v.1.7.6 changes

--- OneSkyBlockSystem.cs
+++ OneSkyBlockSystem.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Linq;
 using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.NetworkInformation;
+using System.Threading.Tasks;
+using System.Threading;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
@@ -496,5 +500,56 @@ namespace OneSkyBlock
                 }
             }
         }
+    }
+    internal class OneSkyBlockUpdate : ModSystem
+    {
+        static readonly HttpClient client = new();
+        static readonly string url = "https://raw.githubusercontent.com/Dummiez/tModLoader-OneSkyBlock/main/build.txt";
+        public override void OnWorldLoad() => CheckLatestVersion();
+        public static void CheckLatestVersion(bool worldLoaded = false)
+        {
+            if (!NetworkInterface.GetIsNetworkAvailable() || Main.netMode == NetmodeID.SinglePlayer && !worldLoaded)
+                return;
+            Task.Run(async () => {
+                while (Main.gameMenu)
+                    Thread.Sleep(new TimeSpan(0, 0, 1));
+                try
+                {
+                    HttpResponseMessage response;
+                    response = await client.GetAsync(url);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var content = await response.Content.ReadAsStringAsync();
+                        var fetchVersion = content.Replace(" ", "");
+                        if (fetchVersion.Contains("version="))
+                        {
+                            fetchVersion = fetchVersion.Split(new[] { "version=" }, StringSplitOptions.None)[1].Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None)[0];
+                            var LatestVersion = new Version(fetchVersion);
+                            if (LatestVersion > OneSkyBlock.Instance.Version)
+                            {
+                                if (worldLoaded)
+                                {
+                                    Main.NewText($"A new version of {OneSkyBlock.Instance.Name} ({OneSkyBlock.Instance.DisplayName}) is available: v{LatestVersion}", Color.LightBlue);
+                                    if (content.Contains("patchNotes = "))
+                                    {
+                                        string patchNote = content.Split(new[] { "patchNotes = " }, StringSplitOptions.None)[1].Split(new[] { "\".*?\"" }, StringSplitOptions.None)[0].Replace("\"", "").Trim('\r', '\n').Replace("\\n", "\r\n");
+                                        Main.NewText(patchNote, Color.LightBlue);
+                                    }
+                                }
+                                else Console.WriteLine($"[{OneSkyBlock.Instance.Name}] A new version is available: v{LatestVersion}");
+                            }
+                        }
+                    }
+                }
+                catch (HttpRequestException e)
+                {
+                    Console.WriteLine($"[{OneSkyBlock.Instance.Name}] {e}");
+                }
+            });
+        }
+    }
+    internal class OneSkyBlockEnter : ModPlayer
+    {
+        public override void OnEnterWorld() => OneSkyBlockUpdate.CheckLatestVersion(true);
     }
 }
